b8fe973b7ab3136879a8f70488d67c9107c295cf
Juan Manuel Sosa <sosaortiz.juanmanuel@hotmail.com>
2024-11-02 17:16:59 -0300

    Se agrega cola para obtener documentos

---
+++ ConvCrmContracts/Conv/Events/MessageReceivedNoClientEvent.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConvCrmContracts.Conv.Events
+{
+    public class MessageReceivedNoClientEvent : IBaseEvent
+    {
+        public string type { get; set; } = default!;
+
+        public string uuid { get; set; } = default!;
+
+        public DateTime timestamp { get; set; }
+
+        public string sender { get; set; } = default!;
+
+        public string receiver { get; set; } = default!;
+
+        public string body { get; set; } = default!;
+
+        public string? source_id { get; set; } = default!;
+
+        public string message_id { get; set; } = default!;
+
+    }
+}
